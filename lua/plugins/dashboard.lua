b47109d1a7c4c4349ef58ec40443348fdbe89939
JEAN Maxime <maxime.jean@deveo.nc>
2024-08-17 16:53:16 +1100

    feat: update config

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -3,7 +3,35 @@ return {
   event = "VimEnter",
   config = function()
     require("dashboard").setup({
-      -- config
+      theme = "hyper",
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          { desc = '󰊳 Lazy Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Find Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = ' Session directory',
+            group = 'DiagnosticHint',
+            action = function() require("persistence").load() end,
+            key = 'sd',
+          },
+          {
+            desc = ' Session last',
+            group = 'DiagnosticHint',
+            action = function() require("persistence").load({last = true}) end,
+            key = 'sl',
+          },
+        },
+      },
     })
   end,
   dependencies = { { "nvim-tree/nvim-web-devicons" } },
