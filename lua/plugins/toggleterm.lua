e7a3824cad5e35a64746701f0e5c5b5676fad927
JEAN Maxime <maxime.jean@deveo.nc>
2024-08-17 15:26:39 +1100

    feat: update toggleterm config

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,17 +2,31 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   config = function()
-    local opts = { buffer = 0 }
-    vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-    vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-    vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-    vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-    vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-    vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-    vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-
-    vim.keymap.set("n", "<Leader>t", "<Cmd>ToggleTerm<CR>", {})
-
     require("toggleterm").setup()
+    -- Define a function to set terminal keymaps
+    function _G.set_terminal_keymaps()
+      local opts = { buffer = 0 } -- Apply mappings only to the current buffer
+      -- Map <esc> to exit terminal mode
+      vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+      -- Map 'jk' to exit terminal mode (useful for typing quickly)
+      vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+      -- Map <C-h>, <C-j>, <C-k>, <C-l> to navigate between windows
+      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+    end
+
+    -- Use an autocommand to apply these mappings when a terminal buffer opens
+    vim.api.nvim_create_autocmd("TermOpen", {
+      pattern = "*", -- Apply to all terminal buffers, or use 'term://*toggleterm#*' for specific
+      callback = function()
+        _G.set_terminal_keymaps()
+      end,
+    })
+    -- user keymap set to toggle terminal
+    vim.keymap.set("n", "<Leader>tt", "<Cmd>ToggleTerm direction=horizontal<CR>", {})
+    vim.keymap.set("n", "<Leader>tf", "<Cmd>ToggleTerm size=10 direction=float<CR>", {})
+    vim.keymap.set("n", "<Leader>tc", "<Cmd>ToggleTerm dir=~/.config/nvim direction=horizontal<CR>", {})
   end,
 }
