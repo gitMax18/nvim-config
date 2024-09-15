0fa3d1acac7420bbb262d10c2348b42b74e6598e
JEAN Maxime <maxime.jean@deveo.nc>
2024-09-15 20:17:58 +1100

    feat: update config

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -1,43 +1,61 @@
 -- used to add and configure debugger
 return {
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-    config = function()
-      local dapui = require("dapui")
-      dapui.setup()
-      local dap = require("dap")
-      -- add event
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-    end,
-  },
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      "leoluz/nvim-dap-go"
-    },
-    config = function()
-      local dap = require("dap")
-      -- add keymaps
-      vim.keymap.set("n", "<Leader>dt", function()
-        dap.toggle_breakpoint()
-      end)
-      vim.keymap.set("n", "<Leader>dc", function()
-        dap.continue()
-      end)
-      -- add specifique debugger
-      require('dap-go').setup()
-    end,
-  },
+	{
+		"rcarriga/nvim-dap-ui",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+		config = function()
+			local dapui = require("dapui")
+			dapui.setup()
+			local dap = require("dap")
+			-- add event
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+		end,
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"leoluz/nvim-dap-go",
+		},
+		config = function()
+			local dap = require("dap")
+			-- add keymaps
+			vim.keymap.set("n", "<Leader>dt", function()
+				dap.toggle_breakpoint()
+			end)
+
+			vim.keymap.set("n", "<Leader>dc", function()
+				dap.continue()
+			end)
+
+			dap.adapters.perl = {
+				type = "executable",
+				-- Path to perl-debug-adapter - will be different based on the installation method
+				-- mason.nvim
+				command = vim.env.MASON .. "/bin/perl-debug-adapter",
+				-- AUR (or if perl-debug-adapter is in PATH)
+				-- command = 'perl-debug-adapter',
+				args = {},
+			}
+
+			dap.configurations.perl = {
+				{
+					type = "perl",
+					request = "launch",
+					name = "Launch Perl",
+					program = "${workspaceFolder}/${relativeFile}",
+				},
+			}
+		end,
+	},
 }
